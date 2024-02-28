ab3d3d6f486800ae89973f175a37e424f1040546
Jhondy Louis <104743895+jdworkz@users.noreply.github.com>
2024-02-28 13:49:06 +0000

    "Added nav link"

--- src/App.js
+++ src/App.js
@@ -4,26 +4,11 @@ import Hero from './components/Hero'
 import Home from './components/Home'
 import Shop from './components/Shop'
 import Contact from './components/Contact'
-import Footer from './components/Footer'
-import About from './components/About'
-import { Route, Routes} from 'react-router-dom'
-
 function App() {
   return (
     <div >
       <Navbar />
       <Hero/>
-      <Routes>
-        <Route path="/Home" element={<Home/>} />
-        <Route path="/Contact" element={<Contact/>} />
-        <Route path="/Shop" element={<Shop/>} />
-        <Route path="/About" element={<About/>} />
-        
-
-      </Routes>
-     
-     
-      <Footer/>
     </div>
   );
 }
